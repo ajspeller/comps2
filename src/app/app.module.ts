7b20a3b2ee74857ee4d7654628092fd09596b327
AJ Speller <ajspeller@gmail.com>
2020-02-06 01:27:17 -0500

    Added Home and Not-found components and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { ElementsModule } from './elements/elements.module';
 import { CollectionsModule } from './collections/collections.module';
 import { ViewsModule } from './views/views.module';
 import { ModsModule } from './mods/mods.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ElementsModule,
     CollectionsModule,
     ViewsModule,
-    ModsModule
+    ModsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
